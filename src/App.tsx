bda630557761039394092d6b4a8bb40291a25b3a
YoasterToaster <joefoschia@gmail.com>
2024-03-18 14:48:46 -0400

    Added components and added constant rotation to cube

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Canvas } from '@react-three/fiber';
+import { Box, PerspectiveCamera } from '@react-three/drei';
+import Cube from './components/cube/Cube';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Canvas>
+      <Cube></Cube>
+    </Canvas>
   );
-}
+};
 
 export default App;
---
+++ src/components/cube/Cube.tsx
@@ -0,0 +1,27 @@
+import React, { useRef } from 'react';
+import { Canvas, useFrame } from '@react-three/fiber';
+import { Box, MeshReflectorMaterial } from '@react-three/drei';
+import { Mesh } from 'three';
+
+const Cube = () => {
+  const cubeRef = useRef<Mesh>(null);
+
+  // Rotate the cube
+  useFrame(() => {
+    if (!cubeRef.current){
+      return;
+    }
+    cubeRef.current.rotation.x += 0.01;
+    cubeRef.current.rotation.y += 0.01;
+  });
+
+  return (
+    <mesh ref={cubeRef}>
+      <Box ref={cubeRef} args={[1, 1, 1]} position={[0, 0, 0]}>
+        <meshStandardMaterial color="red" />
+      </Box>
+      </mesh>
+  );
+};
+
+export default Cube;
